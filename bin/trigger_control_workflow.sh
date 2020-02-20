78525954d7724af85e10b8b0657989a226303b87
Andrey Solovyev <a.solovyev39@gmail.com>
2020-02-20 17:10:21 +0000

    add pipeline trigger script

---
+++ bin/trigger_control_workflow.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cd $EVAL_WORKFLOW_ROOT
+
+# Clone or update the cell-types-eval-workflows repo containing submodules for individual pipelines
+if [ ! -d 'cell-types-eval-workflows' ]; then
+    git clone --recursive https://github.com/ebi-gene-expression-group/cell-types-eval-workflows
+fi
+
+pushd cell-types-eval-workflows > /dev/null
+git checkout $EVAL_WORKFLOW_BRANCH > /dev/null
+git pull > /dev/null
+git submodule update > /dev/null
+popd > /dev/null
+
+
+# run nextflow command 
+nextflow run main.nf -profile cluster 
